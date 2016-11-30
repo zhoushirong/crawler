2472b2c97d5610fbc67257ad66ca8a703069ff10
zhoushirong <1091263031@qq.com>
2016-11-30 13:07:50 +0800

    修改title

--- router/index.js
+++ router/index.js
@@ -9,16 +9,16 @@ router.use(function (req, res, next) {
 var routeArr=['/','/book','/book/:id','/book/:id/:num','/game'];
 router.get(routeArr,function(req, res, next){
 	res.render("index", {
-		title:"欢乐社区",
-		pageKeywords: "欢乐社区,开心一刻,开心一下,烽火戏诸侯,陈二狗的妖孽人生,雪中悍刀行",
-		pageDescription: "大爷，来玩啊~~！",
+		title:"邑抔司",
+		pageKeywords: "邑抔司,开心一刻,开心一下,烽火戏诸侯,陈二狗的妖孽人生,雪中悍刀行",
+		pageDescription: "邑抔司",
 		data: {}
 	});
 });
 router.get('/game/:id',function(req, res, next){
 	let id= req.params.id;
 	res.render(`game/game${id}`, {
-		title:`欢乐社区-游戏${id}`,
+		title:`邑抔司-游戏${id}`,
 		data: {}
 	});
 });
